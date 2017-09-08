95433ddcb0c006480dc011515e4bbfc417ee6745
OneCisco <31762718+OneCisco@users.noreply.github.com>
2017-09-09 00:26:20 +0200

    Create spel-client.py

---
+++ spel-client.py
@@ -0,0 +1,26 @@
+from note import Note # à coder
+from crypto import sign, verify # à coder
+import requests
+
+# pour l'exemple
+URL_SERVER = 'http:/localhost' 
+MY_PRIVATE_KEY = 'XXXX'
+
+# get data for payment # à coder
+merchant_key = 'YYYY'
+data = {'montant': 1, 'cle': merchant_key}
+
+# send a request to the server and get back the note
+r = requests.get(url, data = data)
+response_from_server = r.content() # ou r.json() ?
+note = Note(response_from_server) # il faudra surement reformater response
+
+# verify note
+if not verify(note, note.transactions[-1]['signature']:
+    raise SignatureError # à coder
+
+# create new transaction and sign
+new_transaction = {'cle': merchant_key, 'hash_info_payment': None, 'signature': None}
+note.transactions.append(new_transaction)
+my_sig = sign(note, MY_PRIVATE_KEY)
+note.transactions[-1]['signature'] = my_sig
